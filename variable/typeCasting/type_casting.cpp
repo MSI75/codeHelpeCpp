6f77da2749ab5fd7d4642c4f756b28e663038c46
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-04-14 17:21:28 +0600

    typecasting

---
+++ variable/typeCasting/type_casting.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+        int a = 'a';
+        cout << a << endl;
+
+        char ch = 98;
+        cout << ch << endl;
+
+        char ch1 = 1234567;
+        cout << ch1 << endl;
+
+        int d = -5;
+        cout << d << endl;
+
+        unsigned int e = 121;
+        cout << e << endl;
+
+        unsigned int c = -121;
+        cout << c << endl;
+
+
+    // int a = 2.0 / 5;
+    // float a = 2.0 / 5;
+    double a = 2.0 / 5;
+    cout << a << endl;
+
+
+    int a = 2;
+    int b = 3;
+
+    bool first = (a == b);
+    cout << first << endl;
+
+    bool second = (a > b);
+    cout << second << endl;
+
+    bool third = (a < b);
+    cout << third << endl;
+
+    bool four = (a >= b);
+    cout << four << endl;
+
+    bool five = (a <= b);
+    cout << five << endl;
+
+    bool six = (a != b);
+    cout << six << endl;
+
+
+    int a = 2;
+    cout << !a << endl;
+    */
+    return 0;
+}
