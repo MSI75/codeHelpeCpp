d63a2342a65eb38b07ce89c0408318aab593606f
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-04-15 15:36:25 +0600

    loop

---
+++ loop/while_loop/homeWork.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a, l, sum;
+    cin >> n >> a >> l;
+    sum = n / 2 * (a + l);
+    cout << sum << endl;
+    return 0;
+}
---
+++ loop/while_loop/while_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1, n;
+    cin >> n;
+
+    while (i <= n)
+    {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
