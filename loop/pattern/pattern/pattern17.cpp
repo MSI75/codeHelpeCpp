885bc30988b55278199bc9a67a3c94855119988f
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-04-19 12:45:30 +0600

    homework

---
+++ loop/pattern/pattern/pattern17.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int k = n - i;
+        while (k)
+        {
+            cout << " ";
+            k--;
+        }
+        int j = 1;
+        while (j <= i)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
