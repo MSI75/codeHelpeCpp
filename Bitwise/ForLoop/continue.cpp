340b3379981fbe8b331c0eba97a605a3d1ac4cb4
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-04-20 15:19:42 +0600

    continue

---
+++ Bitwise/ForLoop/continue.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of N " << endl;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        cout << "Hi" << endl;
+        cout << "Hey" << endl;
+
+        continue;
+
+        cout << "reply des na kn " << endl;
+    }
+    return 0;
+}
