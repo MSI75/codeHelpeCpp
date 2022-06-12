f241ebc8e72105c17f52ed67e2d964be7a27eec3
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-06-12 21:21:08 +0600

    Maximum and Minimum

--- Array/maximumOrMinimum.cpp
+++ Array/maximumOrMinimum.cpp
@@ -6,16 +6,54 @@ int getMin(int arr[], int size);
 
 int main()
 {
+    int size;
+    cout << "Index of array : ";
+    cin >> size;
+
+    int arr[100];
+    cout << "Value of array : ";
+    // Taking input an array
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The maximum number of array : " << getMax(arr, size) << endl;
+    cout << "The minimum number of array : " << getMin(arr, size) << endl;
+
+    return 0;
 }
 
 int getMax(int arr[], int size)
 {
-    int max = INT_MAX;
+    int maxi = INT_MIN;
+    // int max = INT_MIN;
     for (int i = 0; i < size; i++)
     {
-        if (arr[i] < max)
-        {
-            max = arr[i];
-        }
+        maxi = max(maxi, arr[i]);
+
+        // if (arr[i] > max)
+        // {
+        //     max = arr[i];
+        // }
     }
+    // returning max value
+    return maxi;
+}
+
+int getMin(int arr[], int size)
+{
+    // int min = INT_MAX;
+    int mini = INT_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        mini = min(mini, arr[i]);
+
+        // if (arr[i] < min)
+        // {
+        //     min = arr[i];
+        // }
+    }
+    // returning min value
+    return mini;
 }
