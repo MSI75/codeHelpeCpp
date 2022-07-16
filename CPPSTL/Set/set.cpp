d1a80b41b9ee5ead3da70514b41bb8d7c214bca8
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-07-16 23:50:49 +0600

    set

---
+++ CPPSTL/Set/set.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    s.insert(7);
+    s.insert(6);
+    s.insert(6);
+    s.insert(6);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    s.insert(3);
+    s.insert(3);
+    s.insert(2);
+
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    set<int>::iterator it = s.begin();
+    it++;
+    s.erase(it);
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "-5 Present or not -> " << s.count(-5) << endl;
+
+    set<int>::iterator itr = s.find(5);
+    cout << "Value present at itr -> " << *it << endl;
+
+    for (auto it = itr; it != s.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
