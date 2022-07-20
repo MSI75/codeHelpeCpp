e472f421b3a00890d193b8b92ca92e2029057db8
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-07-20 18:30:39 +0600

    reverse of string

--- String/LengthOfString.cpp
+++ String/LengthOfString.cpp
@@ -11,6 +11,16 @@ int main()
 
     cout << "My name is ";
 
+    // length size
+    /*
+    int count = 0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+    cout << count;
+    */
+
     cout << getLength(name);
 }
 
