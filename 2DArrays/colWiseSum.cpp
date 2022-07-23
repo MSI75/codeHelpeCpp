613331ef2a7202ca4baaef9c8fb15e1e7ec4a49c
sisaif56 <85629017+sisaif56@users.noreply.github.com>
2022-07-23 10:49:41 +0600

    row wise sum

--- 2DArrays/colWiseSum.cpp
+++ 2DArrays/colWiseSum.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void printRowSum(int arr[][3], int row, int col);
+void printColSum(int arr[][3], int row, int col);
 int main()
 {
     int arr[3][3];
-    // taking input -> row wise input
+
+    // taking input -> col wise array
     cout << "Enter element : ";
     for (int row = 0; row < 3; row++)
     {
         for (int col = 0; col < 3; col++)
         {
-            cin >> arr[row][col];
+            cin >> arr[col][row];
         }
     }
 
     // print
+    cout << "show array : " << endl;
     for (int row = 0; row < 3; row++)
     {
         for (int col = 0; col < 3; col++)
         {
-            cout << arr[row][col] << " ";
+            cout << arr[col][row] << " ";
         }
         cout << endl;
     }
 
-    cout << "Element of row wise sum : " << endl;
-    printRowSum(arr, 3, 3);
+    printColSum(arr, 3, 3);
 }
-
-void printRowSum(int arr[][3], int row, int col)
+void printColSum(int arr[][3], int row, int col)
 {
-    cout << "Row sum : " << endl;
-    for (int row = 0; row < 3; row++)
+    cout << "column sum : " << endl;
+    for (int col = 0; col < 3; col++)
     {
         int sum = 0;
-        for (int col = 0; col < 3; col++)
+        for (int row = 0; row < 3; row++)
         {
-            sum += arr[row][col];
+            sum += arr[col][row];
         }
         cout << sum << " ";
     }
